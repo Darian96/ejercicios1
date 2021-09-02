1585f2d90529baabec6c426531e00f72d586f1f3
Darian96 <you@example.com>
2021-09-02 15:35:30 -0300

    primera practica

---
+++ ejercicios1.php
@@ -0,0 +1,22 @@
+<?php 
+
+$numero = 10;
+$numeroAlCubo = $numero *$numero * $numero;
+
+echo "el $numero al cubo es ",$numeroAlCubo;
+
+//2)
+$fechaAtual = 2021;
+$fechaNacimiento = 1996;
+echo "<br>";
+
+$edad = $fechaAtual - $fechaNacimiento;
+echo  "edad actual es de ",$edad;
+echo "<br>";
+// 3) 
+$distancia = 200;
+$velocidad = 50; 
+$tiempo = $distancia / $velocidad;
+echo $tiempo;
+
+?>
