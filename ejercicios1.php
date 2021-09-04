747161ebe482871f73d4a730f308e089ad1d19f3
Darian96 <you@example.com>
2021-09-03 23:39:34 -0300

    ejercicos 1 completo

--- ejercicios1.php
+++ ejercicios1.php
@@ -18,5 +18,25 @@ $distancia = 200;
 $velocidad = 50; 
 $tiempo = $distancia / $velocidad;
 echo $tiempo;
- //4
+ //5
+ $sueldofijo = 15000;
+  
+ $totalfacturado = $sueldofijo + (5/100 * $sueldofijo);
+echo "<br>";
+echo "total facturado ",$totalfacturado;
+//6
+ $nota1 = 6;
+ $nota2 = 7;
+ $nota3 = 8;
+ echo "<br>";
+$sumaNotas = ($nota1 +$nota2 + $nota3)/3;
+
+echo "promedio general de alumno es ",$sumaNotas;
+echo "<br>";
+$largo = 10;
+$ancho = 5;
+$metroCuadrado = $largo * $ancho;
+echo "total de metrocuadrado ", $metroCuadrado;
+
+
 ?>
